b91e30ecc0788780b55661209ae877c6d1cd375f
Stijn Verholen <stijn.Verholen@vdabcampus.be>
2018-10-11 13:36:49 +0200

    Initial commit - Spring Boot project

---
+++ src/main/java/be/vdab/retrovideo/RetroVideoApplication.java
@@ -0,0 +1,12 @@
+package be.vdab.retrovideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroVideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetroVideoApplication.class, args);
+	}
+}
